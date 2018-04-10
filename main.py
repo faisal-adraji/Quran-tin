318ec3f30bd7dc919f36c640588494c2315697f0
Faisal Adraji <faisal.adraji@gmail.com>
2018-04-10 15:01:59 +0100

    replace and adapt the rest of button's funcs for initializing and binding it in kv file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
 author  :   Faisal Adraji
 email   :   faisal.adraji@gmail.com
+version :   0.0
 start   :   12-2017
 kivy.require("1.9.0")
 '''
@@ -52,12 +53,71 @@ Builder.load_string('''
         size_hint: (.2, .06)
         pos: (200,50)
 
+    MyLabel:
+        id: lbl_scrl_spd
+        text: 'void'
+        markup: True
+        size_hint: (.25, .06)
+        pos: ((400,50))
+
+    MyLabel:
+        id: lbl_viw_siz
+        text: 'void'
+        markup: True
+        size_hint: (.25, .06)
+        pos: (0,50)
+
     Button:
         id: autoscroll_btn
         text: 'autoscroll'
         size_hint: (.15, .05)
         pos_hint: {'x':.425, 'y':.95}
         on_press: root.autoscroll(sv, box)
+
+    Button:
+        id: spd_dwn_btn
+        text: 'spd-'
+        size_hint:(.15, .05)
+        pos_hint: {'x':.7, 'y':.95}
+        on_press: root.spd_dwn(sv, box)
+
+    Button:
+        id: spd_up_btn
+        text: 'spd+'
+        size_hint: (.15, .05)
+        pos_hint: {'x':.15, 'y':.95}
+        on_press: root.spd_up(sv, box)
+        
+    Button:
+        id: goto_page_btn
+        text: 'goto'
+        size_hint: (.15, .05)
+        pos_hint: {'x':.7, 'y':.0}
+        on_press: root.goto_page(sv, box, ti)
+        
+    Button:
+        id: siz_dwn_btn
+        text: 'down'
+        size_hint: (.15, .05)
+        pos_hint: {'x':.1, 'y':.0}
+        on_press: root.siz_dwn(box)
+        
+    Button:
+        id: siz_up_btn
+        text: 'up'
+        size_hint: (.15, .05)
+        pos_hint: {'x':.3, 'y':.0}
+        on_press: root.siz_up(box)
+
+    TextInput:
+        id: ti
+        text: '1'
+        size_hint: (.15, .05)
+        pos_hint: {'x':.5, 'y':.0}
+        multiline: False
+        on_text_validate: root.goto_page
+        # height= '32dp'
+
 
 ''')
 
@@ -74,6 +134,10 @@ class MyLabel(Label):
 
 
 class Tin(FloatLayout):
+
+
+    scrolling = 0
+    step = 1000
     
     #ti = ObjectProperty(None)
 
@@ -84,8 +148,8 @@ class Tin(FloatLayout):
     def update(self, dt):
         bl = self.ids.box
         sv = self.ids.sv
-        lbl_spd = self.children[0]
-        lbl_siz = self.children[1]
+        lbl_spd = self.ids.lbl_scrl_spd
+        lbl_siz = self.ids.lbl_viw_siz
         lbl = self.ids.lbl
 
         #many calculation in the next line of code : 
@@ -117,15 +181,36 @@ class Tin(FloatLayout):
         for i in range(end, LAST_PAGE):
             bl.children[LAST_PAGE-1- i].source = 'free.jpg'
 
+    #functions for buttons
+
     def autoscroll(self, sv, box):
         sv.scroll_to(box.children[0], d= sv.spd)
         print 'this is scroll'
-        #self.scrolling = 1
+        self.scrolling = 1
+
+    def spd_dwn(self, sv, box):
+            sv.spd = sv.spd + self.step
+            if self.scrolling:
+                sv.scroll_to(box.children[0], d= sv.spd)
+            #popup = Popup(title='im scrolling', size_hint=(0.5, 0.5))
+            #popup.open()
+        
+    def spd_up(self, sv, box):
+        sv.spd = sv.spd - self.step
+        if self.scrolling:
+            sv.scroll_to(box.children[0], d= sv.spd)
+    
+    def goto_page(self, sv, box, ti):
+        sv.scroll_to(box.children[LAST_PAGE - int(ti.text)], d=0.2)
+    
+    def siz_up(self, box):
+        box.size_hint[1]+= 10
+    
+    def siz_dwn(self, box):
+        box.size_hint[1]-= 10
 
 class TinApp(App):
 
-    scrolling = 0
-    step = 100
     global Window
 
 
@@ -137,56 +222,22 @@ class TinApp(App):
         #self.title = 'Tin'
         app = Tin(size=(400, 400))
 
-        #finaly found the way for calling obect from kv file
+        #finaly found the way for calling object from kv file
         sv  = app.ids.sv
         box = app.ids.box
-        ti = TextInput(text= '1',size_hint=(.15, .05),pos_hint={'x':.5, 'y':.0}, multiline=False) # height= '32dp'
+        ti  = app.ids.ti
          
         lbl = app.ids.lbl    
-        lbl_scrl_spd = MyLabel(text= 'void', markup= True, size_hint=(.25, .06),pos= (400,50))      
-        lbl_viw_siz = MyLabel(text= 'void', markup= True, size_hint=(.25, .06),pos= (0,50))      
+        lbl_scrl_spd = app.ids.lbl_scrl_spd
+        lbl_viw_siz = app.ids.lbl_viw_siz
 
-    #functions for buttons
+
         
-        def spd_dwn(instance):
-            sv.spd = sv.spd + self.step
-            if self.scrolling:
-                sv.scroll_to(box.children[0], d= sv.spd)
-            #popup = Popup(title='im scrolling', size_hint=(0.5, 0.5))
-            #popup.open()
         
-        def spd_up(instance):
-            sv.spd = sv.spd - self.step
-            if self.scrolling:
-                sv.scroll_to(box.children[0], d= sv.spd)
-        
-        def goto_page(instance):
-            sv.scroll_to(box.children[LAST_PAGE - int(ti.text)], d=0.2)
-        
-        def siz_up(instance):
-            box.size_hint[1]+= 10
-        
-        def siz_dwn(instance):
-            box.size_hint[1]-= 10
         
     # initializing graphic objects
         
-        spd_dwn_btn = Button(text= 'spd-', size_hint=(.15, .05),pos_hint={'x':.7, 'y':.95})
-        spd_dwn_btn.bind(on_press=spd_dwn)
-
-        spd_up_btn = Button(text= 'spd+', size_hint=(.15, .05),pos_hint={'x':.15, 'y':.95})
-        spd_up_btn.bind(on_press=spd_up)
         
-        goto_page_btn = Button(text= 'goto', size_hint=(.15, .05),pos_hint={'x':.7, 'y':.0})
-        goto_page_btn.bind(on_press=goto_page)
-        
-        ti.bind(on_text_validate=goto_page)
-        
-        siz_dwn_btn = Button(text= 'down', size_hint=(.15, .05),pos_hint={'x':.1, 'y':.0})
-        siz_dwn_btn.bind(on_press=siz_dwn)
-        
-        siz_up_btn = Button(text= 'up', size_hint=(.15, .05),pos_hint={'x':.3, 'y':.0})
-        siz_up_btn.bind(on_press=siz_up)
         
         #fill boxlayout with pages
         for i in reversed(range(FIRST_PAGE, LAST_PAGE)):
@@ -194,17 +245,6 @@ class TinApp(App):
             img = Image(source= 'free.jpg')
             box.add_widget(img, len(box.children))
        
-        #fill thee scrollview with the unique boxlayout witch contain all pages
-        #app.add_widget(autoscroll_btn)
-        app.add_widget(spd_dwn_btn)
-        app.add_widget(spd_up_btn)
-        app.add_widget(goto_page_btn)
-        app.add_widget(siz_dwn_btn)
-        app.add_widget(siz_up_btn)
-        app.add_widget(ti)
-        app.add_widget(lbl_scrl_spd)
-        app.add_widget(lbl_viw_siz)
-        #app.add_widget(lbl)
 
         Clock.schedule_interval(app.update, 1.0 / 30.0)
         
